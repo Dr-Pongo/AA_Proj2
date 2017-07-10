51b826681ee4791191d8bf446d58c70f013262cb
Unknown <andrew.rodiek@gmail.com>
2017-07-09 20:30:23 -0400

    Real Read from file in AA_Proj2/AlgNativeSol/Source.cpp

--- AlgNativeSol/AlgNativeSol/Source.cpp
+++ AlgNativeSol/AlgNativeSol/Source.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <iostream>
 #include <string.h>
+#include <fstream>
+#include <string>
 
 #define MATCH 0 /* enumerated type symbol for match */ 
 #define INSERT 1 /* enumerated type symbol for insert */ 
@@ -9,27 +11,25 @@ using namespace std;
 
 const int MAXLEN = 10; /*Added in*/
 
-int string_compare(char *s, char *t, int i, int j);
+int string_compare(const char *s, const char *t, int i, int j);
 int match(char c, char d);
 int indel(char c);
-int arr[10][10] = { -1 };
+int arr[1000][1000] = { -1 };
 
-int wrapper_function() 
+int wrapper_function(string target, string typo) 
 {
-	string abc = "abc";
-	string abc2 = "abcd";
+	//string abc = "abc";
+	//string abc2 = "abcd";
 
-	for (int i = 0; i < 10; i++)
-		for (int j = 0; j < 10; j++)
+	for (int i = 0; i < 1000; i++)
+		for (int j = 0; j < 1000; j++)
 			arr[i][j] = -1;
 
-	//int arr[sizeof(abc)][sizeof(abc2)] = { -1 };
-
-	return string_compare("abc", "abcd", 3, 4);
+	return string_compare(typo.c_str(), target.c_str(), typo.length(), target.length());
 	
 }
 
-int string_compare(char *s, char *t, int i, int j)
+int string_compare(const char*s, const char *t, int i, int j)
 {
 	if (arr[i][j] != -1)
 	{
@@ -77,6 +77,37 @@ int indel(char c)
 }
 
 int main() {
-	cout << wrapper_function() << endl;
+	string typo;
+	string target;
+	string pairs;
+	int numPairs = 0;
+	ifstream inputFile("input.txt");
+
+	if (!inputFile)
+	{
+		cout << "Err reading input.txt" << endl;
+		system("pause");
+		return -1;
+	}
+
+	// Get number of pairs
+	getline(inputFile, pairs, '\n');
+	numPairs = atoi(pairs.c_str());
+
+	// Get input strings
+	for(int i = 0; i < numPairs - 1; i++)
+	{
+		if (inputFile.eof()) // error checking
+		{
+			cout << "Pair number mismatch\n\r Exiting..." << endl;
+			system("pause");
+			return -1;
+		}
+		getline(inputFile, target, '\n');
+		getline(inputFile, typo, '\n');
+		cout << wrapper_function(target, typo) << endl;
+	}
+
+	//cout << wrapper_function() << endl;
 	system("pause");
 }
